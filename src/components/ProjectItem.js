de0c5a44990bb269325e84c991f6747812a96ccf
Dennismaina <mumbidenis@gmail.com>
2024-11-14 05:38:42 +0300

    components

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,28 @@
 import React from "react";
 
+// function ProjectItem({ name, about, technologies }) {
+//   return (
+//     <div className="project-item">
+//       <h3>{name}</h3>
+//       <p>{about}</p>
+//       <div className="technologies">
+//         {<span key={technologies}>{technologies}</span>}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProjectItem;
+
 function ProjectItem({ name, about, technologies }) {
+  const technologiesList = technologies.map((tech) => (
+    <span key={tech}>{tech}</span>
+  ));
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{technologiesList}</div>
     </div>
   );
 }
